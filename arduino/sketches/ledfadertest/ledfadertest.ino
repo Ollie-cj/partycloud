926cfbf93a2d2c745ff1fb9c71b417b6969f2313
Ollie CJ <ollie.cj@gmail.com>
2023-10-28 15:58:21 +0100

    Initial commit following IDE setup Includes test LED fader sketch for XIAO ESP32

---
+++ arduino/sketches/ledfadertest/ledfadertest.ino
@@ -0,0 +1,30 @@
+int ledPin = A10;    // LED connected to digital pin 10
+int micPin = A0;
+
+void setup() {
+  // declaring LED pin as output
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  pinMode(micPin, INPUT);
+}
+
+void loop() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  uint32_t micRead = analogRead(micPin);
+  Serial.println(micRead);
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+}
